b66e3921267587f7d9e11bb51a24261822cc0418
zannatul mawa <zannatulmawa38@gmail.com>
2025-11-11 21:59:47 +0600

    created imported routes

--- src/components/MyImports/MyImports.jsx
+++ src/components/MyImports/MyImports.jsx
@@ -2,7 +2,55 @@ import React from 'react'
 
 const MyImports = () => {
     return (
-        <div>MyImports</div>
+        <div className="overflow-x-auto">
+            <table className="table">
+                {/* head */}
+                <thead>
+                    <tr>
+                        <th>Name and Image</th>
+                        <th>Price</th>
+                        <th>Rating</th>
+                        <th>Origin Country</th>
+                        <th>Available Quantity</th>
+                        <th>Imported Quantity</th>
+                        <th>remove</th>
+                        <th>Details</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {/* row 1 */}
+                    <tr>
+                        <td>
+                            <div className="flex items-center gap-3">
+                                <div className="avatar">
+                                    <div className="mask mask-squircle h-12 w-12">
+                                        <img
+                                            src="https://img.daisyui.com/images/profile/demo/2@94.webp"
+                                            alt="Avatar Tailwind CSS Component" />
+                                    </div>
+                                </div>
+                                <div>
+                                    <div className="font-bold">Hart Hagerty</div>
+                                    <div className="text-sm opacity-50">United States</div>
+                                </div>
+                            </div>
+                        </td>
+                        <td>Price: </td>
+                        <td>Rating</td>
+                        <td>country</td>
+                        <td>Quantity</td>
+                        <td>Imported</td>
+                        <th>
+                            <button className="btn btn-ghost btn-sm">Remove</button>
+                        </th>
+                        <th>
+                            <button className="btn btn-ghost btn-sm"> Details</button>
+                        </th>
+                    </tr>
+
+                </tbody>
+            </table>
+        </div>
     )
 }
 
--- src/components/ProductDetails/ProductDetails.jsx
+++ src/components/ProductDetails/ProductDetails.jsx
@@ -1,34 +1,82 @@
+import { use, useRef } from 'react';
 import { useLoaderData } from 'react-router'
+import { AuthContext } from '../contexts/AuthContext';
 
 const ProductDetails = () => {
     const products = useLoaderData()
-    const { _id, product_name, origin_country, rating, available_quantity, created_at } = products;
-    console.log(products)
+    const importModalRef = useRef(null)
+    const { _id: productId, product_name, origin_country, rating, available_quantity, created_at } = products;
+    const { user } = use(AuthContext)
+    const handleImportModalOpen = () => {
+        importModalRef.current.showModal()
+    }
+    const handleImportSubmit = (e) => {
+        e.preventDefault();
+        const name = e.target.name.value;
+        const email = e.target.email.value;
+        const quantity = e.target.quantity.value;
+        console.log(_id, name, email, quantity);
+
+        const newImportItems = {
+            product: productId
+        }
+    }
     return (
-        <div className=' p-10 flex  justify-around'>
-            <figure>
-                <img className='md:-w-[600px] lg:w-[600px] w-[350px] rounded-lg'
-                    src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-                    alt="Shoes" />
-                <div className='card bg-base-100  shadow-sm lg:w-[600px] w-[350px] md:w-[600px]  mt-4'>
+        <>
+            <div className=' p-10 flex  justify-around'>
+                <figure>
+                    <img className='md:-w-[600px] lg:w-[600px] w-[350px] rounded-lg'
+                        src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                        alt="Shoes" />
+                    <div className='card bg-base-100  shadow-sm lg:w-[600px] w-[350px] md:w-[600px]  mt-4'>
 
-                    <div className='card-body'>
+                        <div className='card-body'>
 
-                        <p>Experience premium quality and performance with the {product_name}. Designed with attention to detail, this product combines durability, modern design, and exceptional functionality to meet your everyday needs.  Whether you’re at home, work, or on the go, the {product_name} ensures convenience and reliability. <br /> It’s the perfect choice for anyone who values comfort, efficiency, and style in one package.</p>
+                            <p>Experience premium quality and performance with the {product_name}. Designed with attention to detail, this product combines durability, modern design, and exceptional functionality to meet your everyday needs.  Whether you’re at home, work, or on the go, the {product_name} ensures convenience and reliability. <br /> It’s the perfect choice for anyone who values comfort, efficiency, and style in one package.</p>
+                        </div>
                     </div>
-                </div>
 
-            </figure>
-            <div className="card bg-base-300  shadow-sm lg:w-[600px] w-[350px] md:w-[600px]">
-                <div className="card-body">
-                    <h2 className="card-title">{product_name}</h2>
-                    <h2 className="card-title  text-sm">Origin Country:{origin_country}</h2>
-                    <h2 className="card-title  text-sm">Rating:{rating}</h2>
-                    <h2 className="card-title  text-sm">Available Quantity:{available_quantity}</h2>
-                    <h2 className="card-title  text-sm">Date:{created_at}</h2>
+                </figure>
+                <div className="card bg-base-300  shadow-sm lg:w-[600px] w-[350px] md:w-[600px]">
+                    <div className="card-body">
+                        <h2 className="card-title text-3xl">{product_name}</h2>
+                        <h2 className="card-title">Origin Country:{origin_country}</h2>
+                        <h2 className="card-title">Rating:{rating}</h2>
+                        <h2 className="card-title">Available Quantity:{available_quantity}</h2>
+                        <h2 className="card-title">Date:{created_at}</h2>
+                    </div>
+                    <button onClick={handleImportModalOpen} className='btn btn-primary'>Import Now!</button>
+                    {/* Open the modal using document.getElementById('ID').showModal() method */}
+                    <dialog ref={importModalRef} className="modal modal-bottom sm:modal-middle">
+                        <div className="modal-box">
+                            <h3 className="font-bold text-lg">Import Product!</h3>
+                            <p className="py-4">fill the details of the  product your want to import</p>
+                            <form onSubmit={handleImportSubmit}>
+                                <fieldset className="fieldset">
+                                    {/* name */}
+                                    <label className="label">Name</label>
+                                    <input readOnly defaultValue={user.displayName} name='name' type="text" className="input w-full" />
+                                    {/* email */}
+                                    <label className="label">Email</label>
+                                    <input readOnly defaultValue={user.email} name='email' type="email" className="input w-full" placeholder="Email" />
+                                    {/* password */}
+                                    <label className="label">Quantity</label>
+                                    <input name='quantity' type="text" className="input w-full" placeholder="add quantity" />
+                                    <button type='submit' className="btn btn-neutral mt-4">Add to Import</button>
+
+                                </fieldset>
+                            </form>
+                            <div className="modal-action">
+                                <form method="dialog">
+                                    <button className="btn">Close</button>
+                                </form>
+                            </div>
+                        </div>
+                    </dialog>
                 </div>
             </div>
-        </div>
+            {/*  */}
+        </>
     )
 }
 
